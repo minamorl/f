8c495237bd866d076f042f90fb46d3eb963460e1
minamorl <minamorl@minamorl.com>
2018-11-03 19:04:22 +0900

    wip

--- src/pages/create.ts
+++ src/pages/create.ts
@@ -26,34 +26,44 @@ const parseAST = (str: string) => {
         content: parser.parse(str),
         visited: false,
     }
+    console.log("==========")
     const dfs = (vertex: IVertex): any => {
-        const queue = []
+        const queue: Array<Micox | string> = []
         vertex.visited = true
+        if (!vertex.content) {
+            return
+        }
+        
         if (!Array.isArray(vertex.content)) {
-            if (typeof vertex.content === "string") {   
+            if (typeof vertex.content === "string") {
                 return vertex.content
             }
-            // queue.push(vertex.content.children)
             if (Array.isArray(vertex.content.children)) {
                 for (const child of vertex.content.children) {
-                    queue.push(new Micox().contains(dfs({content: child, visited: false})).as(vertex.content.tag))
+                    queue.push(new Micox().contains(dfs({content: child, visited: false})).as(child.tag || vertex.content.tag))
                 }
-            }
-            // } else if ("tag" in vertex.content) {
-            //     queue.push(new Micox().contains(dfs({content: vertex.content.children, visited: false})).as(vertex.content.tag))
-            // } 
+            } else if (vertex.content) {
+                queue.push(new Micox().contains(dfs({content: vertex.content.children, visited: false})).as(vertex.content.tag))
+            } 
             return queue
         }
         for (const x of vertex.content) {
-            console.log(x)
-
-            if (typeof x === "string") {   
-                return x
-            }
-            if (Array.isArray(x.children)) {
+            if (typeof x === "string") {
+                queue.push(x)
+            } else if (Array.isArray(x.children) && x.children.length) {
+                queue.push(new Micox().contains(dfs({content: x.children, visited: false})).as(x.tag))
+            } else if (Array.isArray(x.children)) {
                 for (const child of x.children) {
-                    queue.push(new Micox().contains(dfs({content: child, visited: false})).as(x.tag))
+                    if (child.args === null && child.children === null) {
+                        console.log("here")
+                        queue.push(new Micox().contains().as(child.tag)) // br tag
+                    } else {
+                        console.log("here?")
+                        queue.push(new Micox().contains(dfs({content: child.children, visited: false})).as(child.tag))
+                    }
                 }
+            } else if (Array.isArray(x)) {
+                queue.push(dfs({content: x, visited: false}))
             } else if ("tag" in x) {
                 queue.push(new Micox().contains(dfs({content: x.children, visited: false})).as(x.tag))
             } 
@@ -62,6 +72,7 @@ const parseAST = (str: string) => {
         return queue
     }
     const proceed = dfs(parsed)
+    console.log(proceed)
     return proceed
 }
 
@@ -69,7 +80,7 @@ export const createPage = (props: IDestructedURL) => (portal: Portal) => {
     const preview = html.div().class("preview")
     return html.div([
         html.h1("Create new document"),
-        html.textarea().attrs({ placeholder: "Insert here" }).events({
+        html.textarea("abc\ndef\nghj").attrs({ placeholder: "Insert here" }).events({
             keyup: ev => {
                 const parsed = parseAST(ev.target.value)
                 portal.transfer("preview", parsed)
